c3384d982e63ff4383a555c7388131389a605140
alekskivuls <akivuls@gmail.com>
2017-11-16 22:14:32 -0800

    Implement basic search

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -1,35 +1,61 @@
 package indexer
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"encoding/json"
 	"log"
-	"bytes"
-	"time"
+	"strings"
+	"text/scanner"
 )
 
+//Mapping vocab terms to document names
 type InvertedIndex struct {
-	data map[string]interface{}
+	index map[string][]string
 }
 
-func Index(files []string) (InvertedIndex, time.Duration)  {
-	start := time.Now();
+type Document struct {
+	Title  string
+	Author string
+	Body   string
+}
+
+func Index(files []string) InvertedIndex {
 	var index InvertedIndex
+	index.index = make(map[string][]string)
+	var doc Document
 	for _, file := range files {
-		fmt.Println(file)
 		raw, _ := ioutil.ReadFile(file)
-		if err := json.Unmarshal(raw, &index.data); err != nil {
+		if err := json.Unmarshal(raw, &doc); err != nil {
 			log.Fatal(err)
 		}
+		tokenize(index, doc.Body, file)
 	}
-	return index, time.Now().Sub(start)
+	return index
+}
+
+func tokenize(index InvertedIndex, text string, docName string) {
+	var s scanner.Scanner
+	s.Init(strings.NewReader(text))
+	for token := s.Scan(); token != scanner.EOF; token = s.Scan() {
+		//fmt.Printf("%d: %s\n", s.Position.Offset, s.TokenText())
+		index.index[s.TokenText()] = append(index.index[s.TokenText()], docName)
+	}
 }
 
 func (index InvertedIndex) String() string {
 	var buffer bytes.Buffer
-	for key, value := range index.data {
+	for key, value := range index.index {
 		buffer.WriteString(fmt.Sprint(key, ": ", value, "\n"))
 	}
 	return buffer.String()
+}
+
+func (index InvertedIndex) Size() int {
+	return len(index.index)
+}
+
+func (index InvertedIndex) Search(query string) []string {
+	return index.index[query]
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/alekskivuls/jsonSearchEngine/common"
 	"github.com/alekskivuls/jsonSearchEngine/indexer"
@@ -10,7 +11,15 @@ import (
 
 func main() {
 	files := common.GetJsonFilesFromPath(os.Args[1:])
-	index, duration := indexer.Index(files)
-	fmt.Println("Took", duration, "to index")
-	fmt.Print(index)
+	start := time.Now()
+	fmt.Println(files)
+	index := indexer.Index(files)
+	fmt.Println("Took", time.Now().Sub(start), "to index")
+	fmt.Println(index.Size())
+
+	fmt.Print("Enter search term: ")
+	var input string
+	fmt.Scanln(&input)
+	fmt.Print(input)
+	fmt.Println(index.Search(input))
 }
